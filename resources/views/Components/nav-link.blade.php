1eb247f36714fdcca40cc6e0442d77d18d92eb49
locate <integrityms@gmail.com>
2024-05-27 12:52:52 +0900

    라라벨 공부 5일 마무리 네비게이션에 Active 속성을 추가하여 CSS 변경 하는 방법 확인

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,1 +1,3 @@
-<a {{ $attributes }}>{{ $slot }}</a>
+{{-- 전달받는 $attributes 중 active 속성을 가지며 기본값은 false로 정의. --}}
+@props(['active' => false ])
+<a class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium" aria-current="{{ $active ? 'page': 'false' }}" {{$attributes}} >{{$slot}}</a>
